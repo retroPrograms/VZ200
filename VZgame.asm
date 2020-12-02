94562786a4194991bad82b5d6625505fc08996d1
retroPrograms <mcarvalho_au@hotmail.com>
2020-12-02 21:26:48 +1100

    Initial Commit VZgame

---
+++ VZgame.asm
@@ -0,0 +1,120 @@
+;g2 start
+.org 32256
+	
+start:
+	;CALL 01C9h	;clear screen
+	call hires
+	ld hl,07000h
+	call drawTile
+	ld hl,07004h
+	call drawTile
+	ld hl,07008h
+	call drawTile
+	;ld hl,07250h
+	;call undrawTile
+	ld hl,07000h
+	call undrawTile
+	call readkey
+	CALL 01C9h	;clear screen
+	ret
+	
+	
+undrawTile:
+	ld c,4
+	ld b,12
+	;ld de, tile1
+	;ld hl, tileStart ;07000h
+tileLoop1:
+	ld a,0
+	ld (hl),a
+	inc hl
+	;inc de
+	dec c
+	jp nz, tileLoop1
+	;push de
+	ld de, 28
+	add hl, de
+	;pop de
+	ld c,4
+	dec b
+	jp nz, tileLoop1
+	ret
+	
+	
+drawTile:
+	ld c,4
+	ld b,12
+	ld de, tile1
+	;ld hl, tileStart ;07000h
+tileLoop:
+	ld a,(de)
+	ld (hl),a
+	inc hl
+	inc de
+	dec c
+	jp nz, tileLoop
+	push de
+	ld de, 28
+	add hl, de
+	pop de
+	ld c,4
+	dec b
+	jp nz, tileLoop
+	ret
+
+		
+
+
+	
+hires:
+		ld hl, 06800h
+		ld a,8
+		ld (hl), a
+		ld hl,077FFh
+		ld a,06fh
+clearLoop:
+			ld (hl), 0
+			dec l
+			jp nz, clearLoop
+			dec h
+			cp h
+			jp nz, clearLoop
+			ret
+
+		
+readkey:
+scan:  ;registers af, bc, de and hl 
+	call 2ef4h
+	cp 84
+	jp z, erase
+	cp 020h 
+	jr nz,scan
+	CALL 01C9h	;clear screen
+	ret
+erase:
+	ld hl,07250h
+	call undrawTile
+	jp scan
+tile1 	.byte 0,0,0,0
+	    .byte 12,255,255,255
+		.byte 51,255,255,255
+		.byte 255,255,255,255
+		.byte 255,255,255,255
+		.byte 255,255,255,255
+		.byte 255,255,255,255
+		.byte 255,255,255,255
+		.byte 255,255,255,255
+		.byte 255,255,255,255
+		.byte 255,255,255,255
+		.byte 255,255,255,255
+
+tileStart:
+		.byte 2
+		
+;number .byte 1
+;singlenum	.byte 1
+;hexnum .byte 1
+
+;keyboard .byte 1
+
+	.end
